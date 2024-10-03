d797551f827abfd7a8274922c0bdd9be74a79813
oussama <oussamabechichi@gmail.com>
2024-10-03 18:04:19 +0200

    added cors and imported external routes in main file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import cors from "cors";
 import express, { Express } from "express";
 import mongoose from "mongoose";
+import AuthRouter from "./routes/auth";
+import UserRouter from "./routes/users";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/test";
 
@@ -12,6 +16,9 @@ mongoose
   .connect(mongoURI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Failed to connect to MongoDB", err));
+
+app.use("/auth", AuthRouter);
+app.use("/users", UserRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
